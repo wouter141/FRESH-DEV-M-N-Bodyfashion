31ca20191c489277ab9af72995d91da3f6911ea3
wouter141 <32423696+wouter141@users.noreply.github.com>
2019-02-27 13:57:05 +0100

    Added start of footer

--- Site/components/Footer.php
+++ Site/components/Footer.php
@@ -1,3 +1,14 @@
-<?php
-  echo "Hi";
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Footer</title>
+    <link rel="stylesheet" href="FooterCSS.css">
+  </head>
+  <body>
+  </body>
+  <footer>
+    <div class="footer">
+    </div>
+  </footer>
+</html>
