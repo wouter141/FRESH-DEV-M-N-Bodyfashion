ca906fa7289d95d6d2700660e6c5588ec6146a83
thesmallwan <12321.post@gmail.com>
2019-02-27 13:34:17 +0100

    Begin van de navigatie

---
+++ Site/components/Navbar.php
@@ -0,0 +1,23 @@
+<nav>
+  <div class="Nav-container">
+      <a href="./index.php">
+          <img src="" alt="">
+      </a>
+      <a  href="./OverOns.php">
+          <span>Wie wij zijn</span>
+      </a>
+      <a href="./Merken.php">
+          <span>Alle merken</span>
+      </a>
+      <a href="./Spaarsysteem.php">
+          <span>Spaarsysteem</span>
+      </a>
+      <a href="./Acties.php">
+          <span>Actie</span>
+      </a>
+      <a href="./Contact.php">
+          <span>Contact</span>
+      </a>
+
+  </div>
+</nav>
--- Site/index.php
+++ Site/index.php
@@ -1,5 +1,15 @@
-<?php
-  include './components/Footer.php';
-  include './components/Header.php';
-  include './components/Navbar.php';
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="./components/nav.css">
+  </head>
+  <body>
+    <?php
+      include './components/Footer.php';
+      include './components/Header.php';
+      include './components/Navbar.php';
+    ?>
+  </body>
+</html>
