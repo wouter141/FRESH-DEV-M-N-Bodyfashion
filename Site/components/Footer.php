d6251aa6bd6107e59e84776016b45e18b8ea8987
wouter141 <32423696+wouter141@users.noreply.github.com>
2019-03-11 10:58:40 +0100

    Changes in footer

--- Site/components/Footer.php
+++ Site/components/Footer.php
@@ -1,10 +1,6 @@
-
   <footer>
     <div class="footer">
       <div class="footeropening">
       <img src="./components/images/Logo.png"></img>
-      <div class="footeropening">
-        <h3>Openingstijden</h3>
-      </div>
     </div>
   </footer>
