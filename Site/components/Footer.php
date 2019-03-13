b42155586febe7956440a08b86736821d9ac29e6
wouter141 <32423696+wouter141@users.noreply.github.com>
2019-03-13 13:03:31 +0100

    Changes to footer

--- Site/components/Footer.php
+++ Site/components/Footer.php
@@ -1,31 +1,22 @@
 <footer id="footer">
   <div class="container">
-    <!-- Row -->
 			<div class="row">
         <div class="col-md-12">
-           <!-- footer logo -->
 					<div class="footer-logo">
-						<img src="../../../components/images/Logo.png">
+						<img src="components/images/Logo.png">
 					</div>
-					<!-- /footer logo -->
-					<!-- footer follow -->
 					<ul class="footer-follow">
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-						<li><a href="#"><i class="fa fa-instagram"></i></a></li>
-						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-						<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+						<li><a class="linkfooter" href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a class="linkfooter" href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a class="linkfooter" href="#"><i class="fa fa-google-plus"></i></a></li>
+						<li><a class="linkfooter" href="#"><i class="fa fa-instagram"></i></a></li>
+						<li><a class="linkfooter" href="#"><i class="fa fa-linkedin"></i></a></li>
+						<li><a class="linkfooter" href="#"><i class="fa fa-youtube"></i></a></li>
 					</ul>
-					<!-- /footer follow -->
-					<!-- footer copyright -->
 					<div class="footer-copyright">
-						<p>Copyright © 2016-2018. All Rights Reserved. <a href="http://scanfcode.com/" target="_blank">Scanfcode</a></p>
+						<p>Copyright ©2019. All Rights Reserved. <a href=" https://fresh-dev.nl/" target="_blank">FRESH-DEV</a></p>
 					</div>
-					<!-- /footer copyright -->
 				</div>
 			</div>
-			<!-- /Row -->
 		</div>
-		<!-- /Container -->
 	</footer>
