ddf67ab021180cdfb7ac757831caa91e4b970081
thesmallwan <12321.post@gmail.com>
2019-03-13 09:29:21 +0100

    Changes aan de navbar

--- Site/components/javascript/index.js
+++ Site/components/javascript/index.js
@@ -2,11 +2,9 @@ $(function(){
   $(window).scroll(function() {
      if($(window).scrollTop() >= 100) {
        $('nav').addClass('scrolled');
-       document.getElementByTagName("nav").style.color = "blue";
      }
     else {
       $('nav').removeClass('scrolled');
-      document.getElementByTagName("nav").style.color = "blue";
     }
   });
 });
