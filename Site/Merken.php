3c281b3f67e9c2db5bebb5658baf6897fca1f038
wouter141 <32423696+wouter141@users.noreply.github.com>
2019-03-20 13:48:55 +0100

    Merken changes

--- Site/Merken.php
+++ Site/Merken.php
@@ -16,50 +16,7 @@
       <h2 class="Main-Paragraaf">Of u nu op zoek bent naar Lingerie, badmode of nachtmode, M&N Bodyfashion heeft het allemaal voor zowel dames als heren!
           De volgende merken kunt u bij M&N Bodyfashion vinden: </h2>
           <hr>
-      <div class="container">
-    <div class="selected-item">
-      <p>Je Hebt Geselecteerd: <span id="spanHeading">Leeg</span></p>
     </div>
-
-    <div class="dropdown">
-      <span class="selLabel">Selecteer Kledingtype</span>
-      <ul class="dropdown-list">
-        <li data-value="1" onclick="myFunction()">
-          <span id="Lingerie">Lingerie</span>
-        </li>
-        <li data-value="2">
-          <span>Badmode</span>
-        </li>
-        <li data-value="3">
-          <span>Nachtmode</span>
-        </li>
-        <li data-value="4">
-          <span>Accessoires</span>
-        </li>
-        <li data-value="5">
-          <span>Shapewear</span>
-        </li>
-      </ul>
-    </div>
-    <div class="dropdown-content">
-      <div id="content-Lingerie">
-        aaa
-      </div>
-      <div class="kleding content-Badmode">
-
-      </div>
-      <div class="kleding content-Accessoires">
-
-      </div>
-      <div class="kleding content-Nachtmode">
-
-      </div>
-      <div class="kleding content-Shapewear">
-
-      </div>
-    </div>
-    </div>
-  </div>
   </div>
   </body>
   <?php include('./components/Footer.php'); ?>
