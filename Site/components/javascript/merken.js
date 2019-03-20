d0731297677f17518021d03d9796542855c5bde7
wouter141 <32423696+wouter141@users.noreply.github.com>
2019-03-20 10:06:36 +0100

    Added Javascript and changed merken page

---
+++ Site/components/javascript/merken.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $(".selLabel").click(function () {
+    $('.dropdown').toggleClass('active');
+  });
+
+  $(".dropdown-list li").click(function() {
+    $('.selLabel').text($(this).text());
+    $('.dropdown').removeClass('active');
+    $('.selected-item p span').text($('.selLabel').text());
+  });
+
+});
