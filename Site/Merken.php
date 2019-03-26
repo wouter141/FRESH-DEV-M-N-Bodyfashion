d300f92843a350c3652d87aae1655ba7b09184d5
thesmallwan <12321.post@gmail.com>
2019-03-26 09:16:13 +0100

    Responsiveness op Merken.php aan het testen

--- Site/Merken.php
+++ Site/Merken.php
@@ -16,22 +16,25 @@
       <h2 class="Main-Paragraaf">Of u nu op zoek bent naar Lingerie, badmode of nachtmode, M&N Bodyfashion heeft het allemaal voor zowel dames als heren!
           De volgende merken kunt u bij M&N Bodyfashion vinden: </h2>
           <hr>
-          <div class="dropdown">
-            <button class="dropbtn">Heren</button>
-            <div class="dropdown-content">
-              <a onclick="divVisibility('dropdowncontent-heren-ondermode');">Ondermode</a>
-              <a onclick="divVisibility('dropdowncontent-heren-nachtmode');">Nachtmode</a>
-              <a onclick="divVisibility('dropdowncontent-heren-badmode');">Badmode</a>
+          <div class="dropdownAll">
+
+            <div class="dropdown">
+              <button class="dropbtn">Heren</button>
+              <div class="dropdown-content">
+                <a onclick="divVisibility('dropdowncontent-heren-ondermode');">Ondermode</a>
+                <a onclick="divVisibility('dropdowncontent-heren-nachtmode');">Nachtmode</a>
+                <a onclick="divVisibility('dropdowncontent-heren-badmode');">Badmode</a>
+              </div>
             </div>
-          </div>
-          <div class="dropdowndames">
-            <button class="dropbtndames">Dames</button>
-            <div class="dropdown-contentdames">
-              <a onclick="divVisibility('dropdowncontent-dames-lingerie');">Lingerie</a>
-              <a onclick="divVisibility('dropdowncontent-dames-badmode');">Badmode</a>
-              <a onclick="divVisibility('dropdowncontent-dames-nachtmode');">Nachtmode</a>
-              <a onclick="divVisibility('dropdowncontent-dames-accessoires');">Accessoires</a>
-              <a onclick="divVisibility('dropdowncontent-dames-shapewear');">Shapewear</a>
+            <div class="dropdowndames">
+              <button class="dropbtndames">Dames</button>
+              <div class="dropdown-contentdames">
+                <a onclick="divVisibility('dropdowncontent-dames-lingerie');">Lingerie</a>
+                <a onclick="divVisibility('dropdowncontent-dames-badmode');">Badmode</a>
+                <a onclick="divVisibility('dropdowncontent-dames-nachtmode');">Nachtmode</a>
+                <a onclick="divVisibility('dropdowncontent-dames-accessoires');">Accessoires</a>
+                <a onclick="divVisibility('dropdowncontent-dames-shapewear');">Shapewear</a>
+              </div>
             </div>
           </div>
           <div id="dropdowncontent-heren-ondermode">
