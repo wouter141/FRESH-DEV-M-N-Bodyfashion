deb3f81d21526a84fa1d6f786f03b9bbb354d5c7
thesmallwan <12321.post@gmail.com>
2019-03-27 10:25:27 +0100

    Acties Alleen nog foto in zetten

--- Site/Acties.php
+++ Site/Acties.php
@@ -12,6 +12,17 @@
         <h1>Acties</h1>
       </div>
       <div class="Main-Paragraaf">
+        <div class="ActiesTop">
+          <button type="button" name="button" class="ActiesButtons"><p>Zomer Acties!</p></button>
+          <img src="./components/images/index2.jpg" alt="IMAGE INSTERT HERE" class="ImageActies">
+          <button type="button" name="button" class="ActiesButtons"><p>Winter Acties!</p></button>
+        </div>
+        <div class="ActiesOnder">
+          <h1>Verloting</h1>
+            <button type="button" name="button" class="ActiesButtonsOnder"><p>Kijk of U ook heeft gewonnen!</p></button>
+        </div>
       </div>
+    </div>
+    <?php include('./components/Footer.php'); ?>
   </body>
 </html>
