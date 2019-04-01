a84c54de19742620d2bb75a65b930ae5aa2fcf7d
wouter141 <32423696+wouter141@users.noreply.github.com>
2019-04-01 10:56:30 +0200

    Meer lijst changes

--- Site/Lijst.php
+++ Site/Lijst.php
@@ -15,7 +15,7 @@
           <h1>Prijswinnaars verloting lingerieset</h1>
         </div>
       <div class="container-flex">
-        <div class="prijs-lingerieset">
+        <div class="prijswinnaars">
           <ul>
             <li>Zaterdag 29-8:</li>
             <li>Zondag 30-8:</li>
@@ -28,7 +28,7 @@
             <li>Zondag 6-9:</li>
           </ul>
         </div>
-        <div class="prijs-lingerieset">
+        <div class="prijswinnaars">
           <ul>
             <li>mevr. I. Kan</li>
             <li>mevr. G. de Wildt</li>
@@ -39,8 +39,65 @@
             <li>mevr. S. Jonk</li>
             <li>mevr. M. Poland</li>
             <li>mevr. L. de Waal</li>
+          </ul>
         </div>
-        <div>3</div>
+        <div class="prijswinnaars">
+          <ul>
+            <li>bonnr. 80397</li>
+            <li>bonnr. 80412</li>
+            <li>bonnr. 80433</li>
+            <li>bonnr. 80453</li>
+            <li>bonnr. 80492</li>
+            <li>bonnr. 80534</li>
+            <li>bonnr. 80586</li>
+            <li>bonnr. 80647</li>
+            <li>bonnr. 80755</li>
+          </ul>
+        </div>
+      </div>
+      <div class="prijswinnaars-text prijswinnaars-text2">
+        <h1>Prijswinnaars verloting herenshort</h1>
+      </div>
+      <div class="container-flex">
+          <div class="prijswinnaars">
+          <ul>
+            <li>Zaterdag 29-8:</li>
+            <li>Zondag 30-8:</li>
+            <li>Maandag 31-8:</li>
+            <li>Dinsdag 1-9:</li>
+            <li>Woensdag 2-9:</li>
+            <li>Donderdag 3-9:</li>
+            <li>Vrijdag 4-9:</li>
+            <li>Zaterdag 5-9:</li>
+            <li>Zondag 6-9:</li>
+          </ul>
+        </div>
+        <div class="prijswinnaars">
+          <ul>
+            <li>mevr. M. Lourens</li>
+            <li>mevr. F. Kramer</li>
+            <li>mevr. L. Schoester</li>
+            <li>mevr. B. van Wijnbergen</li>
+            <li>mevr. A.A.M. Poel</li>
+            <li>dhr. B. Gest</li>
+            <li>mevr. M. Turkenburg:</li>
+            <li>dhr. Peppels</li>
+            <li>mevr. M van Tiel</li>
+          </ul>
+        </div>
+        <div class="prijswinnaars">
+          <ul>
+            <li>Zaterdag 29-8:</li>
+            <li>Zondag 30-8:</li>
+            <li>Maandag 31-8:</li>
+            <li>Dinsdag 1-9:</li>
+            <li>Woensdag 2-9:</li>
+            <li>Donderdag 3-9:</li>
+            <li>Vrijdag 4-9:</li>
+            <li>Zaterdag 5-9:</li>
+            <li>Zondag 6-9:</li>
+          </ul>
+        </div>
       </div>
       </div>
     </div>
