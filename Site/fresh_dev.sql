41910d4b4539d94e5c25d30ebcfee238f7646155
wouter141 <32423696+wouter141@users.noreply.github.com>
2019-04-15 09:03:38 +0200

    sql statement verandering

--- Site/fresh_dev.sql
+++ Site/fresh_dev.sql
@@ -1,37 +1,116 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2019 at 03:08 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
- Source Server         : Fresh_Dev
- Source Server Type    : MySQL
- Source Server Version : 100130
- Source Host           : localhost:3306
- Source Schema         : fresh_dev
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100130
- File Encoding         : 65001
 
- Date: 08/04/2019 10:49:36
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `fresh_dev`
+--
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `username` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `email` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `admin` tinyint(1) NOT NULL DEFAULT 0,
-  UNIQUE INDEX `username`(`username`(20)) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('Ivar', 'test123', '12321.post@gmail.com', 1);
+--
+-- Table structure for table `images`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+CREATE TABLE `images` (
+  `id` smallint(99) NOT NULL,
+  `image_naam` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `image_naam`) VALUES
+(2, 'image.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `text`
+--
+
+CREATE TABLE `text` (
+  `id` smallint(99) NOT NULL,
+  `string` mediumtext
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` mediumtext NOT NULL,
+  `password` mediumtext NOT NULL,
+  `mail` tinytext NOT NULL,
+  `admin` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `mail`, `admin`) VALUES
+('Ivar', 'test123', '12321.post@gmail.com', 1),
+('Wouter', 'test123', 'woutertest@gmail.com', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `text`
+--
+ALTER TABLE `text`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`(99));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` smallint(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `text`
+--
+ALTER TABLE `text`
+  MODIFY `id` smallint(99) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
