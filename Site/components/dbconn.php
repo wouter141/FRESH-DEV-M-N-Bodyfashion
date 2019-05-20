49acb0aa6e3e455969511ebdf921e91a88c1c1ac
thesmallwan <12321.post@gmail.com>
2019-05-20 09:02:39 +0200

    dbconn update

--- Site/components/dbconn.php
+++ Site/components/dbconn.php
@@ -1,6 +1,15 @@
 <?php
-		$dsn = 'mysql:host=fresh-dev.academy;dbname=ivar_wouter';
-		$username = "ivar";
-		$password = '$Vl537ja';
-        $conn = new PDO($dsn, $username, $password);
+
+//Server
+//		$dsn = 'mysql:host=fresh-dev.academy;dbname=ivar_wouter';
+//		$username = "ivar";
+//		$password = '$Vl537ja';
+//        $conn = new PDO($dsn, $username, $password);
+
+
+//Localhost
+$dsn = 'mysql:host=localhost;dbname=fresh_dev';
+$username = "root";
+$password = '';
+$conn = new PDO($dsn, $username, $password);
 ?>
